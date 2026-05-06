1dd89d1abab8dac701adb83b7c22e477568b2d49
美都 <facemito@laki.jp>
2026-05-06 19:24:35 +0900

    feat: systemd-notifyを実装 (#4)
    * chore: 依存クレートのupdate
    
    * feat: systemd-notifyを実装
    
    - systemd-notifyを実装。これで、systemdのtypeをnotifyにできるようになり、ExecStartPostが正確に実行できるようになりました。
    
    * style: ログメッセージの共通化。及び文言修正

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,15 @@ fn main() {
             })
         })
         .unwrap();
+        let window_initialized_message = "ウィンドウの初期化を終了しました。";
+        info!("{}", window_initialized_message);
+        sd_notify::notify(&[
+            sd_notify::NotifyState::Ready,
+            sd_notify::NotifyState::Status(window_initialized_message),
+        ])
+        .unwrap_or_else(|e| {
+            error!("systemdへの通知に失敗しました。:{}", e);
+        });
     });
     info!("針時計を終了しました。");
 }
